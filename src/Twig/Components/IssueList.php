98b27f2c77a4e45f90386c64496843b15fba515f
xmarchegay <xmarchegay@clever-age.com>
2025-02-05 20:20:02 +0100

    add search issues

--- src/Twig/Components/IssueList.php
+++ src/Twig/Components/IssueList.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
+use function Symfony\Component\String\s;
 
 #[AsLiveComponent]
 final class IssueList
@@ -20,12 +21,15 @@ final class IssueList
     #[LiveProp]
     public ?int $project_id = null;
 
+    #[LiveProp(writable: true)]
+    public string $query = '';
+
     public string $title = 'Issues';
 
     public function __construct(
         private readonly JiraInstanceRepository $repository,
-        private readonly Jira $jira,
-        private readonly RequestStack $requestStack,
+        private readonly Jira                   $jira,
+        private readonly RequestStack           $requestStack,
     )
     {
     }
@@ -42,6 +46,30 @@ final class IssueList
         usort($issues, static function ($a, $b) {
             return strcmp($b['fields']['updated'], $a['fields']['updated']);
         });
+
+        if ($this->query !== '') {
+            $issues = array_filter($issues, function (array $issue) {
+                $key = $issue['key'];
+                $title = $issue['fields']['summary'];
+                $status = $issue['fields']['status']['name'];
+                $prio = $issue['fields']['priority']['name'];
+                $component = !empty($issue['fields']['components']) ? $issue['fields']['components'][0]['name'] : '';
+                $fixVersion = !empty($issue['fields']['fixVersions']) ? $issue['fields']['fixVersions'][0]['name'] : '';
+
+                $found = true;
+                foreach (explode(' ', $this->query) as $str) {
+                    $found = $found &&
+                        (s($key)->ignoreCase()->startsWith($str) ||
+                            s($title)->ignoreCase()->containsAny($str) ||
+                            s($status)->ignoreCase()->containsAny($str) ||
+                            s($prio)->ignoreCase()->containsAny($str) ||
+                            s($component)->ignoreCase()->containsAny($str) ||
+                            s($fixVersion)->ignoreCase()->containsAny($str)
+                        );
+                }
+                return $found;
+            });
+        }
 
         return $issues;
     }
